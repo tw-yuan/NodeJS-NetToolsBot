13f9fd418bb581e571c3183985288ad5b904de2a
YuanYuan <85051500+yuan2005@users.noreply.github.com>
2022-11-12 13:32:22 +0800

    m

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-var ping = require('ping');
+const fs = require("fs");
+const ms = require('ms');
+const schedule = require('node-schedule');
+const request = require("request");
+const { exec } = require("child_process");
+const express = require('express');
+const app = express();
+const ping = require('ping');
 
-var hosts = ['192.168.1.1', 'google.com', 'yahoo.com'];
+app.get('/ping', function (req, res) {
+    var host = req.query.host;
 
-hosts.forEach(function (host) {
+    res.send("Not Auth");
+
     ping.promise.probe(host)
         .then(function (res) {
             console.log(res);
+            res.send(res);
         });
 });
+
+app.listen(26001, function () {
+    console.log('FAD後端已啟用於26001端口!');
+});
+//防崩潰
+process.on('uncaughtException', error => {
+    console.log(error)
+});
+
+process.on('unhandledRejection', error => {
+    console.log(error)
+})
