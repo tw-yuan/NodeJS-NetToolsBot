925de15dff96349e7043928998b8692391e5c086
YuanYuan <85051500+yuan2005@users.noreply.github.com>
2022-11-12 23:49:55 +0800

    up

--- functions/API.js
+++
@@ -1,5 +0,0 @@
-const request = require("request");
-
-module.exports = function () {
-	request("網址", function(error, response, body) {});
-}
--- functions/Invite_Create.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-
-module.exports = function (invite) {
-	var data = JSON.parse(fs.readFileSync(__dirname + "/../data/invites.json"));
-	data[invite.guild.id][invite.code] = 0;
-	fs.writeFileSync(__dirname + "/../data/invites.json", JSON.stringify(data, null, '\t'));
-}
